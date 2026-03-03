0fcfcedf85b01871475a960f3c8a0b16be7bd3cf
bintangmogot <harunbintangmogot07@gmail.com>
2026-03-03 13:10:52 +0800

    fix: change public storage path to handle InfinityFree limit

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => base_path('unduhan'),
+            'url' => env('APP_URL') . '/unduhan',
             'visibility' => 'public',
             'throw' => false,
         ],
